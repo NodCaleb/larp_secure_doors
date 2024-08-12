e55afddebdc077ac9d712f0e34158e4c70adce9d
NodCaleb <nod.caleb@gmail.com>
2024-08-12 22:10:04 +0300

    Added helper app

---
+++ Helper/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var codes = File.ReadAllLines(@"D:\Sources\larp_secure_doors\codes.txt");
+
+var arrays = new List<string>();
+
+foreach (var code in codes)
+{
+	arrays.Add($"{{'{code[0]}', '{code[1]}', '{code[2]}', '{code[3]}'}},");
+}
+
+File.WriteAllLines(@"D:\Sources\larp_secure_doors\arrays.txt", arrays);
+
+Console.WriteLine("Cmoplete!");
